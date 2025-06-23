f5822bd0363b15941ee82615479e68a2d7489241
Sayantan Roy <itsroysayantan@gmail.com>
2025-06-23 14:33:14 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MovieCard from "./components/MovieCard"
+function App() {
+
+  return (
+  <>
+    <MovieCard movie={{title: "Roy", release_date:"14 December 2002"}}/>
+  </>
+  )
+}
+
+
+export default App;
---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+function MovieCard({movie}) {
+
+    function Favoriteclick() {
+        alert("clicked")
+    }
+    
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title} />
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onClick={Favoriteclick}>
+                        ❤ 
+                    </button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
