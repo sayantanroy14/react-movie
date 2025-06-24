7a9907fe0c284f8778196a30e62ed55c46a2bad6
Sayantan Roy <itsroysayantan@gmail.com>
2025-06-24 23:16:30 +0530

    updated project and did not hardcode api key

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
-import MovieCard from "./components/MovieCard"
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
+
 function App() {
-
   return (
-  <>
-    <MovieCard movie={{title: "Roy", release_date:"14 December 2002"}}/>
-  </>
-  )
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
 }
-
 
 export default App;
